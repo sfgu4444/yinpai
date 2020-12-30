1e9a79a5b81bb7961e3e995bd1d07ec0a69269c8
刘振达 <liuzd@yuntongxun.com>
2020-12-30 11:28:15 +0800

    1 日志

---
+++ server/src/main/java/com/yinpai/server/log/WebLog.java
@@ -0,0 +1,15 @@
+package com.yinpai.server.log;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+@Documented
+public @interface WebLog {
+    /**
+     * 日志描述信息
+     *
+     * @return
+     */
+    String description() default "";
+}
